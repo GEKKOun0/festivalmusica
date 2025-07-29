a5116b85bcffb76a3773de216a3b3f8ea196a081
Juan Ramon <gekkoone7@gmail.com>
2025-07-29 12:20:09 -0600

    Primer commit añadiendo el gitignore

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+import { src, dest, watch} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+export function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError))//esto hace que al finalizar la funcion anterior se ejecute este justo despues, para llevar un orden
+        .pipe( dest('build/css'))
+        
+    done();
+
+}
+
+export function dev() {
+    watch('src/scss/**/*.scss', css)
+}
