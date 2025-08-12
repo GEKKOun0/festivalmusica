1842c79d6d7fd92ee69280fc6c0312cd0be0fd71
Juan Ramon <gekkoone7@gmail.com>
2025-08-12 16:35:05 -0600

    Añadiendo la galeria interactriva con js y ajustando el efecto scroll

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
-import { src, dest, watch} from 'gulp'
+import { src, dest, watch, series} from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
-export function css( done ) {
-    src('src/scss/app.scss' , {sourcemaps: true})
-        .pipe( sass().on('error', sass.logError))//esto hace que al finalizar la funcion anterior se ejecute este justo despues, para llevar un orden
-        .pipe( dest('build/css', {sourcemaps: true}) )
-        
+export function js(done) {
+
+    src('src/js/script.js')
+        .pipe(dest('build/js') )
+
+    done();
+}
+
+export function css(done) {
+    src('src/scss/app.scss', { sourcemaps: true })
+        .pipe(sass().on('error', sass.logError))//esto hace que al finalizar la funcion anterior se ejecute este justo despues, para llevar un orden
+        .pipe(dest('build/css', { sourcemaps: true }))
+
     done();
 
 }
 
 export function dev() {
     watch('src/scss/**/*.scss', css)
+    watch('src/js/**/*.js', js)
 }
+
+export default series(js, css, dev);//parallel para arrancar las tareas al mismo tiempo en lugar de una por una con series
